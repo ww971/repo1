6425ec7bee1dc4ec7c66b2c9af3e8693e3b9272b
ww971 <ww8@interia.pl>
2021-01-05 12:24:40 +0100

    doc

---
+++ doc/html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['myexp_2eh_5',['myexp.h',['../myexp_8h.html',1,'']]]
+];
---
+++ doc/html/search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['main_7',['main',['../myexp_8h.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'myexp.h']]],
+  ['myexp_8',['myexp',['../myexp_8h.html#a3d4b0f79727d3dd508a4870b1c6c0e3a',1,'myexp.h']]]
+];
